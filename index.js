ed3769fd51732c9f000ce05c392d3dccd9ecbe12
CH-Fathima-Almas <70479704+CH-Fathima-Almas@users.noreply.github.com>
2022-07-25 11:47:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+router.get('/signup', (req, res) => {
+  res.render('signup');
+});
+module.exports = router;
