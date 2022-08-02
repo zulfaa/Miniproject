e2e0b1051b28d9ea414d2fb2918d0080eb27bfe8
zulfaa <70093603+zulfaa@users.noreply.github.com>
2022-08-02 21:29:01 +0530

    changes made

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-var express = require('express');
-var router = express.Router();
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/login', (req, res) => {
-  res.render('login');
-});
-router.get('/signup', (req, res) => {
-  res.render('signup');
-});
-module.exports = router;
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "main.html"; // Redirecting to other page.
+return false;
+}
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
